90bc85e761c891dfd74d0adcde05358a6dd1e698
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-12-07 04:22:08 +0530

    Handling POST Request For Login

--- users/views.py
+++ users/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render 
+from django.shortcuts import render   
+from django.http import HttpResponse
+from django.contrib.auth import authenticate , login
 from .forms import LoginForm
 
 # Create your views here. 
 
-def user_login(request): 
-    form = LoginForm() 
+def user_login(request):  
+    if request.method == "POST": 
+        form = LoginForm(request.POST) 
+        if form.is_valid():  
+            data = form.cleaned_data 
+            user = authenticate(request,username=data['username'],password=data['password']) 
+            if user is not None: 
+                login(request,user) 
+                return HttpResponse("user athenticated and logged in")
+            else: 
+                return HttpResponse("Invalid credentials")
+    else:     
+        form = LoginForm() 
     return render(request,'users/login.html',{'form':form})
