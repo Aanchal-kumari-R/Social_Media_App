4754662a969c2898e3e60c78a9a8499f0a120657
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-12-10 05:44:50 +0530

    Password Reset Done View

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -123,4 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
 LOGIN_URL = 'login' 
-LOGOUT_URL = 'logout'
+LOGOUT_URL = 'logout' 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), 
          name="password_change_done"), 
     path('password_reset/',auth_view.PasswordResetView.as_view(template_name="users/password_reset_form.html"), 
-         name="password_reset"),
+         name="password_reset"),  
+    path('password_reset/done/',auth_view.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), 
+         name="password_reset_done"),
 ]
