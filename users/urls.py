c747552bc1a3fb72427f35394c8dc46f0d429f73
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-12-13 04:55:13 +0530

    Creating User Registration Form

--- users/urls.py
+++ users/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), 
          name="password_reset_confirm"), 
     path('reset/done/',auth_view.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), 
-         name="password_reset_complete"),
+         name="password_reset_complete"), 
+    path('register/',views.register,name='register')
 ]
