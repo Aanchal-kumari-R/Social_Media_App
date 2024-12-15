a82e715d3ed17ce9d6896fa68f8825a21175a183
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-12-16 05:14:12 +0530

    Creating Index Page in Users App

--- users/views.py
+++ users/views.py
@@ -6,7 +6,8 @@ from .forms import LoginForm , UserRegistrationForm
 from django.contrib.auth import logout 
 from django.contrib.auth.decorators import login_required
 from .models import Profile 
-from .forms import UserEditForm, ProfileEditForm
+from .forms import UserEditForm, ProfileEditForm 
+from posts.models import Post
 # Create your views here. 
 
 def user_login(request):  
@@ -29,8 +30,10 @@ def logout_view(request):
     return render(request,'users/logout.html') 
 
 @login_required
-def index(request): 
-    return render(request,'users/index.html') 
+def index(request):  
+    current_user = request.user 
+    posts = Post.objects.filter(user=current_user)
+    return render(request,'users/index.html',{'posts':posts}) 
 
 def register(request): 
     if request.method == 'POST': 
@@ -46,7 +49,7 @@ def register(request):
     return render(request,'users/register.html',{'user_form':user_form}) 
 
 @login_required
-def edit(request): 
+def edit(request):  
     if request.method == 'POST': 
         user_form = UserEditForm(instance=request.user,data=request.POST) 
         profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)  
