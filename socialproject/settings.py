277d473efa8d7e4b8c190d6f365b7dc0b71670f6
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-12-15 05:42:13 +0530

    Post Model

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [ 
-    'users',
+    'users', 
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
